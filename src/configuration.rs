e4a277bc0410c1ff3928a4cabe92ac6686e2bc98
Nate Finger <nfinger1020@gmail.com>
2024-07-07 21:51:44 -0400

    Cant get the ssl to work

--- src/configuration.rs
+++ src/configuration.rs
@@ -29,17 +29,17 @@ pub struct DatabaseSettings {
 
 impl DatabaseSettings {
     pub fn without_db(&self) -> PgConnectOptions {
-        let ssl_mode = if self.require_ssl {
-            PgSslMode::Require
-        } else {
-            PgSslMode::Prefer
-        };
+        // let ssl_mode = if self.require_ssl {
+        //     PgSslMode::Require
+        // } else {
+        //     PgSslMode::Prefer
+        // };
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
             .password(self.password.expose_secret())
             .port(self.port)
-            .ssl_mode(ssl_mode)
+            // .ssl_mode(ssl_mode)
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
