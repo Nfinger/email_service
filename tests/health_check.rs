b75f51d09220312be83e236de9daa2028af7acac
Nate Finger <nfinger1020@gmail.com>
2024-07-13 20:33:30 -0400

    parse form and add domain

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -138,14 +138,14 @@ async fn subscribe_returns_a_400_for_invalid_form_data() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
-        ("name=&email=test%40gmail.com", "empty name"),
-        ("name=Test&email=", "empty email"),
-        ("name=Test&email=definitely-not-an-email", "invalid email")
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
     ];
 
     for (body, description) in test_cases {
@@ -160,9 +160,9 @@ async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
 
         // Assert
         assert_eq!(
-            200,
+            400,
             response.status().as_u16(),
-            "The API did not return a 200 OK when the payload was {}.",
+            "The API did not return a 400 Bad Request when the payload was {}.",
             description
         );
     }
